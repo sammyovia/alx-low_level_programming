feab8673a01ab550cc32e36d5c5bf3153ff11a57
sammyovia <sammyigbinovia>
2022-06-30 19:45:45 +0000

    adding 5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,7 @@
+#include<stdio.h>                                     
+/**
+ * main - Entry point
+ *                                                     * Return: Always 0 (Success)                          * Description:prints a message to the screen using printf
+ *                                                     */
+int main(void)                                        {                                                             printf("with proper grammer, but the outcome is a piece of art,\n");                                               return (0);
+}
