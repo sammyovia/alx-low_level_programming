85c6b0b33eb9c605a8d42209002812b55b23054c
sammyovia <sammyigbinovia>
2022-06-30 19:15:49 +0000

    hotfix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,12 @@
  * main - Entry point
  *
  * Return:
+ * Descripion: prints a message to the output using pput
+ *
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);/*using return in main ends the program*/
+	return (0);
 }
