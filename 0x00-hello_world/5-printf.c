fc4de00e9997f476ba57702c6dd5c87fbb67acfa
sammyovia <sammyigbinovia>
2022-06-30 20:01:43 +0000

    hotfix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
 #include<stdio.h>                                     
+
 /**
  * main - Entry point
  *                                                     * Return: Always 0 (Success)                          * Description:prints a message to the screen using printf
 */
-int main(void)                                        {	
+
+int main(void)
+{	
 	printf("with proper grammer, but the outcome is a piece of art,\n");	
 	return (0);
 }
