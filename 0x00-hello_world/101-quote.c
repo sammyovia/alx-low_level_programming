7f75e67f464f97f2c9721794544347a66a546891
sammyovia <sammyigbinovia@gmail.com>
2022-07-01 05:37:25 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include<stdio.h>                                                                                           /**                                                    * main - Entry point                                  *                                                     * Return: Always 1 (Success)                          * Description: prints out the last part of a quote
 */
 int main(void)                                        {
-	write(2, "
-and that piece of art is useful- Dora Korpar, 2015-10-19\n" 58);
+	fwrite(2, "
+and that piece of art is useful"\"- Dora Korpar, 2015-10-19\n" 58);
 	return (1);
