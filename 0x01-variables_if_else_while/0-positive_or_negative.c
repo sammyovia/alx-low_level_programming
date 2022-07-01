140c1901e7029cfc7f9baa677556dc3a25b60b4d
sammyovia <sammyigbinovia@gmail.com>
2022-07-01 18:36:29 +0000

    hotfix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
-/* Description : A description for function main */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * Description: Return function main for rand */
 int main(void)
 {
 	int n;
