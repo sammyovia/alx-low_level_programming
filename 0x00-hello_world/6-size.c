aa02b1f64165a9c3a453c9bfc12b5e773b91183d
sammyovia <sammyigbinovia@gmail.com>
2022-07-01 04:08:21 +0000

    hotfix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	float floatType;
 	double doubleType;
 	char charType;
-	
+       
 	printf("Size of int: %zu bytes\n", sizeof(intType));
 	printf("Size of float: %zu bytes\n", sizeof(floatType));
 	printf("Size of double: %zu bytes\n", sizeof(doubleType));
