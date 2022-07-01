b20e9460e9d854ad228ed5db29b093eb6950bb12
sammyovia <sammyigbinovia@gmail.com>
2022-07-01 05:18:23 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,3 @@
 #include<stdio.h>                                                                                           /**                                                    * main - Entry point                                  *                                                     * Return: Always 1 (Success)                          * Description: prints a message to the screen using scanf
 */
-int main(void)                                        {                                                             scanf("and that piece of art is useful" \\"- Dora Korpar, 2015-10-19\n");                                               return (1);
+int main(void)                                        {                                                             scanf("and that piece of art is useful"\- Dora Korpar, 2015-10-19\n");                                               return (1);
