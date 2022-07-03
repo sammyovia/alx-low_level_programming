34fdf8cb3beb64318f4345e1ed2ca72cc5e73418
sammyovia <sammyigbinovia@gmail.com>
2022-07-03 20:39:16 +0000

    hotfix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
-
+#include <stdio.h>
 /**                                                    * main - Entry point                                  *                                                     * Return: Always success one
  * Description: prints out the last part of a quote
  *
