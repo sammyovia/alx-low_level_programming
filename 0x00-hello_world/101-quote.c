5ec8fa79ae09ec9c201d900d54a34a0a544bec4e
sammyovia <sammyigbinovia@gmail.com>
2022-07-03 19:38:19 +0000

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-/**                                                    * main - Entry point                                  *                                                     * Return: Always 1 (Success)                          * Description: prints out the last part of a quote
-*/
+
+/**                                                    * main - Entry point                                  *                                                     * Return: Always success one
+ * Description: prints out the last part of a quote
+ *
+ */
 int main(void)                                        {
-	char *string;
-	char str1[] =(" and that piece of art is useful\"");
-	char str2 =(" - Dora Korpar, 2015-10-2019");
-	string = malloc(strlen(str1) + strlen(str2) +1);
-	strcpy(string, str1);
-	strcpy(string, str2);
-	write(2, string, sizeof(string));
+	write(2, "and that piece of art is useful \"- Dora Korpar, 2015-10-2019\n", 59);
 	return (1);
 }
