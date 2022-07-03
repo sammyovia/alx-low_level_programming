316e0f66fce67722f326a418a24ec5602c6081db
sammyovia <sammyigbinovia@gmail.com>
2022-07-03 00:04:52 +0000

    adding 101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,17 +15,17 @@ int main(void)
 		for (e2 = e1 + 1; e2 < 9; e2++)
 		{
 			for (e3 = e2 + 1; e3 < 10; e3++)
+			{
+				putchar((e1 % 10) + '0');
+				putchar((e2 % 10) + '0');
+				putchar((e3 % 10) + '0');
+				if (e1 == 7 && e2 == 8 && e3 == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
 		}
-		putchar((e1 % 10) + '0');
-		putchar((e2 % 10) + '0');
-		putchar((e3 % 10) + '0');
-		if (e1 == 7 && e2 == 8 && e3 == 9)
-			continue;
-		putchar(',');
-		putchar(' ');
 	}
-}
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+	return (0);
 }
