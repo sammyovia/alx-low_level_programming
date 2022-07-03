5e22fb087020c299584a151120a9edd784fcb92f
sammyovia <sammyigbinovia@gmail.com>
2022-07-03 17:55:06 +0000

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,14 @@ int main(void)
 {
 	int i = 0;
 
-	for (i = 10; i < 99; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		putchar(i);
+		putchar((i % 10) + '0');
+
+		if (i == 9)
+			continue;
 		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
