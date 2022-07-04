0448274140ce4bdcea5edb67b606cf714ba88a2e
Sammyovia <107253576+sammyovia@users.noreply.github.com>
2022-07-04 14:24:54 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,6 @@ int main(void)
 	float afloat;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(achar));
-
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(aint));
 	printf("Size of a long int : %lu byte(s)\n", (unsigned long)sizeof(along));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(alonglong));
