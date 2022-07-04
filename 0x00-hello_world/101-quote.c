3ece06f6d0577a436eb00d73da0db423cc45700d
sammyovia <sammyigbinovia@gmail.com>
2022-07-04 14:15:54 +0000

    hotfix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,14 @@
 #include <unistd.h>
 #include <stdio.h>
-/**                                                    * main - Entry point                                  *                                                     * Return: Always success one
- * Description: prints out the last part of a quote
+/**                                                    * main - Entry point                                
+ * 
+ * Return: Always success one
+ * Description:prints out the last part of a quote
  *
  */
-int main(void)                                        {
-	write(1, "and that piece of art is useful \"- Dora Korpar, 2015-10-2019\n", 59);
+int main(void)
+{
+	write(1, "and that piece of art is useful\"- Dora Korpar, 2015-10-2019\n", 59);
+
 	return (1);
 }
