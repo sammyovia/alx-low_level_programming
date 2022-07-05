c76d714ca5e47229409ad2b3b9753283c30fe9dd
sammyovia <sammyigbinovia@gmail.com>
2022-07-05 18:49:16 +0000

    hotfix

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/** 
+ * Return: 1 if in is lowercase 0 if not
+ * Description: checks for lowercase character
+ */
+int is_lower(int l)
+{
+	if (c >= 'a' && l <= 'z')
+		return (1);
+	else
+		return (0);
+}
