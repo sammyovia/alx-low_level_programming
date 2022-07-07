05059e0a62985e107daea2a4002f24f932ff58cc
sammyovia <sammyigbinovia@gmail.com>
2022-07-07 19:25:00 +0000

    update

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,6 +9,7 @@
 void print_diagonal(int n)
 {
 	int a, b;
+
 	if (n <= 0)
 		_putchar('\n');
 	for (a = 0; a < n; a++)
@@ -17,7 +18,7 @@ void print_diagonal(int n)
 		{
 			_putchar(' ');
 		}
-	       	_putchar('\\');
-	       	_putchar('\n');
+		_putchar('\\');
+		_putchar('\n');
 	}
 }
