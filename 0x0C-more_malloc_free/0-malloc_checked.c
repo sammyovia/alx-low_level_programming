f72f70ce2e5eb609725ab831af3fa4f4eee20609
sammyovia <sammyigbinovia@gmail.com>
2022-07-29 02:04:58 +0000

    hotfix to a

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,40 +1,22 @@
-#include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
-* string_nconcat - prints concatenate string;
-* @s1: input string.
-* @s2: input string.
-* @n: len s2 string for print.
-* Return: Nothing.
-*/
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+ * malloc_checked - fuction that allocates memory and also checks if sucessful
+ * @b: size of memory to allocate.
+ *
+ * Return: pointer to memory, Else if return exit status 98.
+ */
+void *malloc_checked(unsigned int b)
 {
-	unsigned int l1, i, e;
-	char *a;
+	void *ptr;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	l1 = 0;
-	while (s1[l1])
-		l1++;
-	a = malloc(sizeof(*a) * l1 + n + 1);
-
-	if (a == NULL)
-		return (NULL);
-	for (i = 0, e = 0; i < (l1 + n); i++)
+	ptr = malloc(b);
+	if (ptr == NULL)
 	{
-		if (i < l1)
-		{
-			a[i] = s1[i];
-		}
-		else
-		{
-			a[i] = s2[e++];
-		}
+		free(ptr);
+		exit(98);
 	}
-	a[i] = '\0';
-	return (a);
+	return (ptr);
 }
+
