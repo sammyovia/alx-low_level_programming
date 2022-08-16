f2e9d4f3fa97afc5d1d46bfe907bdd98efc3f907
sammyovia <sammyigbinovia@gmail.com>
2022-08-16 22:16:12 +0000

    fix lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,15 +1,13 @@
-#ifndef LISTS_H
-#define LISTS_H
-#include <stddef.h>
-
+#ifndef HOLBERTON
+#define HOLBERTON
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for Holberton project
  */
-
 typedef struct listint_s
 {
 	int n;
@@ -24,7 +22,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
 int pop_listint(listint_t **head);
-listint_t *get_nodeint_at_in dex(listint_t *head, unsigned int index);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
@@ -33,5 +31,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 void _printd(int n);
-
 #endif
